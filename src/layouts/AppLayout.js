91f69617b6eb9ad56c698dc8b31346c580378ce7
chirath <chirath.didulanga@axiatadigitallabs.com>
2025-11-29 23:36:19 +0530

    initial layout created

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -1,12 +1,55 @@
+import {
+    AppBar, Box, CssBaseline, Drawer,
+
+    Toolbar,
+    Typography
+} from "@mui/material";
 
 
 
 const AppLayout = () => {
 
-
     return (
         <>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                    sx={{
+                        width: { sm: `calc(100% - 240px)` },
+                        ml: { sm: `240px` }
+                    }}
+                >
+                    <Toolbar>
 
+                        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+                            Inventory Dashboard
+                        </Typography>
+
+
+                    </Toolbar>
+                </AppBar>
+                <Box
+                    component="nav"
+                    sx={{ width: { sm: `240px` }, flexShrink: { sm: 0 } }}
+                    aria-label="navigation"
+                >
+
+                    <Drawer
+                        variant="permanent"
+                        sx={{
+                            display: { xs: 'none', sm: 'block' },
+                            '& .MuiDrawer-paper': {
+                                boxSizing: 'border-box',
+                                width:`240px`
+                            }
+                        }}
+                        open
+                    >
+
+                    </Drawer>
+                </Box>
+            </Box>
         </>
     );
 };
