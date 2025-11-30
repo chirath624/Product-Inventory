c9671d1eba26ef0bd59b9cc787ecf72600be8ed7
chirath <chirath.didulanga@axiatadigitallabs.com>
2025-11-30 10:58:03 +0530

    created the nav bar

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -1,14 +1,46 @@
 import {
-    AppBar, Box, CssBaseline, Drawer,
+    AppBar, Box, CssBaseline, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText,
 
     Toolbar,
     Typography
 } from "@mui/material";
-
-
+import { NavLink, Outlet } from 'react-router-dom';
+import {useMemo} from "react";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import InventoryIcon from '@mui/icons-material/Inventory';
+const navItems = [
+    {
+        to: '/products',
+        label: 'Products',
+        icon: <InventoryIcon />
+    },
+    {
+        to: '/orders',
+        label: 'Orders',
+        icon: <ShoppingCartIcon />
+    }
+];
 
 const AppLayout = () => {
 
+    const drawer = (
+        <div>
+            <Toolbar>
+                <Typography variant="h6">Inventory Admin</Typography>
+            </Toolbar>
+            <Divider />
+            <List>
+                {navItems.map((item) => (
+                    <ListItem key={item.to} disablePadding>
+                        <ListItemButton component={NavLink} to={item.to}>
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.label} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
     return (
         <>
             <Box sx={{ display: 'flex' }}>
@@ -46,9 +78,10 @@ const AppLayout = () => {
                         }}
                         open
                     >
-
+                        {drawer}
                     </Drawer>
                 </Box>
+
             </Box>
         </>
     );
